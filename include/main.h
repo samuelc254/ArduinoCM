9320a25a2d2ba2c9d7d22284efd9aa3bc7bbcec9
Samuel Costa <sam.oc2005@gmail.com>
2022-10-30 09:46:53 -0300

    add more uno shortcuts

--- include/main.h
+++ include/main.h
@@ -31,47 +31,17 @@ const char *macros = R"(
 const char *configString = R"( 
 {
     "UNO": {
-        "0": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
+        "8": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
             "bit": "0"
         },
-        "1": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "1"
+        "D8": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "0"
         },
-        "2": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "2"
-        },
-        "3": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "3"
-        },
-        "4": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "4"
-        },
-        "5": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "5"
-        },
-        "6": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "6"
-        },
-        "7": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "7"
-        },
-        "8": {
+        "PB0": {
             "wPort": "PORTB",
             "rPort": "PINB",
             "bit": "0"
@@ -81,7 +51,32 @@ const char *configString = R"(
             "rPort": "PINB",
             "bit": "1"
         },
+        "D9": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "1"
+        },
+        "PB1": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "1"
+        },
         "10": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "2"
+        },
+        "D10": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "2"
+        },
+        "PB2": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "2"
+        },
+        "SS": {
             "wPort": "PORTB",
             "rPort": "PINB",
             "bit": "2"
@@ -91,7 +86,37 @@ const char *configString = R"(
             "rPort": "PINB",
             "bit": "3"
         },
+        "D11": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "3"
+        },
+        "PB3": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "3"
+        },
+        "MOSI": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "3"
+        },
         "12": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "4"
+        },
+        "D12": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "4"
+        },
+        "PB4": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "4"
+        },
+        "MISO": {
             "wPort": "PORTB",
             "rPort": "PINB",
             "bit": "4"
@@ -101,7 +126,37 @@ const char *configString = R"(
             "rPort": "PINB",
             "bit": "5"
         },
+        "D13": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "5"
+        },
+        "PB5": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "5"
+        },
+        "SCK": {
+            "wPort": "PORTB",
+            "rPort": "PINB",
+            "bit": "5"
+        },
         "14": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "0"
+        },
+        "D14": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "0"
+        },
+        "PC0": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "0"
+        },
+        "A0": {
             "wPort": "PORTC",
             "rPort": "PINC",
             "bit": "0"
@@ -111,7 +166,37 @@ const char *configString = R"(
             "rPort": "PINC",
             "bit": "1"
         },
+        "D15": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "1"
+        },
+        "PC1": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "1"
+        },
+        "A1": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "1"
+        },
         "16": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "2"
+        },
+        "D16": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "2"
+        },
+        "PC2": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "2"
+        },
+        "A2": {
             "wPort": "PORTC",
             "rPort": "PINC",
             "bit": "2"
@@ -121,7 +206,42 @@ const char *configString = R"(
             "rPort": "PINC",
             "bit": "3"
         },
+        "D17": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "3"
+        },
+        "PC3": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "3"
+        },
+        "A3": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "3"
+        },
         "18": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "4"
+        },
+        "D18": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "4"
+        },
+        "PC4": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "4"
+        },
+        "A4": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "4"
+        },
+        "SDA": {
             "wPort": "PORTC",
             "rPort": "PINC",
             "bit": "4"
@@ -131,235 +251,155 @@ const char *configString = R"(
             "rPort": "PINC",
             "bit": "5"
         },
-        "A0": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "0"
-        },
-        "A1": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "1"
-        },
-        "A2": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "2"
-        },
-        "A3": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "3"
-        },
-        "A4": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "4"
-        },
-        "A5": {
+        "D19": {
             "wPort": "PORTC",
             "rPort": "PINC",
             "bit": "5"
-        },
-        "PD0": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "0"
-        },
-        "PD1": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "1"
-        },
-        "PD2": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "2"
-        },
-        "PD3": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "3"
-        },
-        "PD4": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "4"
-        },
-        "PD5": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "5"
-        },
-        "PD6": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "6"
-        },
-        "PD7": {
-            "wPort": "PORTD",
-            "rPort": "PIND",
-            "bit": "7"
-        },
-        "PB0": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "0"
-        },
-        "PB1": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "1"
-        },
-        "PB2": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "2"
-        },
-        "PB3": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "3"
-        },
-        "PB4": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "4"
-        },
-        "PB5": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "5"
-        },
-        "PC0": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "0"
-        },
-        "PC1": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "1"
-        },
-        "PC2": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "2"
-        },
-        "PC3": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "3"
-        },
-        "PC4": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "4"
         },
         "PC5": {
             "wPort": "PORTC",
             "rPort": "PINC",
             "bit": "5"
         },
+        "A5": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "5"
+        },
+        "SCL": {
+            "wPort": "PORTC",
+            "rPort": "PINC",
+            "bit": "5"
+        },
+        "0": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "0"
+        },
         "D0": {
             "wPort": "PORTD",
             "rPort": "PIND",
             "bit": "0"
+        },
+        "PD0": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "6"
+        },
+        "RX": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "0"
+        },
+        "1": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "1"
         },
         "D1": {
             "wPort": "PORTD",
             "rPort": "PIND",
             "bit": "1"
         },
+        "PD1": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "7"
+        },
+        "TX": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "1"
+        },
+        "2": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "2"
+        },
         "D2": {
             "wPort": "PORTD",
             "rPort": "PIND",
             "bit": "2"
+        },
+        "PD2": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "2"
+        },
+        "INT0": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "2"
+        },
+        "3": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "3"
         },
         "D3": {
             "wPort": "PORTD",
             "rPort": "PIND",
             "bit": "3"
         },
+        "PD3": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "3"
+        },
+        "INT1": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "3"
+        },
+        "4": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "4"
+        },
         "D4": {
             "wPort": "PORTD",
             "rPort": "PIND",
             "bit": "4"
+        },
+        "PD4": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "4"
+        },
+        "5": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "5"
         },
         "D5": {
             "wPort": "PORTD",
             "rPort": "PIND",
             "bit": "5"
         },
+        "PD5": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "5"
+        },
+        "6": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "6"
+        },
         "D6": {
             "wPort": "PORTD",
             "rPort": "PIND",
             "bit": "6"
         },
-        "D7": {
+        "7": {
             "wPort": "PORTD",
             "rPort": "PIND",
             "bit": "7"
         },
-        "D8": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "0"
-        },
-        "D9": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "1"
-        },
-        "D10": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "2"
-        },
-        "D11": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "3"
-        },
-        "D12": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "4"
-        },
-        "D13": {
-            "wPort": "PORTB",
-            "rPort": "PINB",
-            "bit": "5"
-        },
-        "D14": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "0"
-        },
-        "D15": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "1"
-        },
-        "D16": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "2"
-        },
-        "D17": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "3"
-        },
-        "D18": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "4"
-        },
-        "D19": {
-            "wPort": "PORTC",
-            "rPort": "PINC",
-            "bit": "5"
+        "D7": {
+            "wPort": "PORTD",
+            "rPort": "PIND",
+            "bit": "7"
         }
     }
 }
